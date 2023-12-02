20869b4768f525baa0b6ccfc600a6bcd5b79414f
Mariusz Dąbrowski <57808812+marioooo0o@users.noreply.github.com>
2023-12-02 11:45:50 +0200

    feat: add swagger OpenAPI3.0

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -6,6 +6,25 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="Filmweeb API Documentation",
+ *      description="Description of available endpoints of Filmweeb API",
+ *      @OA\Contact(
+ *          email="admin@admin.com"
+ *      ),
+ *      @OA\License(
+ *          name="Apache 2.0",
+ *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *      )
+ * )
+ *
+ * @OA\Server(
+ *      url=L5_SWAGGER_CONST_HOST,
+ *      description="Filmweeb API Server"
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
--- backend/app/Http/Controllers/GetTitleController.php
+++ backend/app/Http/Controllers/GetTitleController.php
@@ -12,6 +12,30 @@ use Illuminate\Http\Request;
 
 class GetTitleController extends BaseController
 {
+    /**
+     * @OA\Get (
+     * path="/title/{title_id}",
+     * summary="Get by title",
+     * description="URL from provider, get access token",
+     * operationId="providerAccess",
+     * tags={"Authentication"},
+     * @OA\RequestBody(
+     *    required=true,
+     *    description="Pass provider name",
+     *    @OA\JsonContent(
+     *       required={"provider name"},
+     *       @OA\Property(property="provider_name", type="string", example="github"),
+     *    ),
+     * ),
+     * @OA\Response(
+     *    response=422,
+     *    description="Wrong credentials response",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="message", type="string", example="Sorry, wrong email address or password. Please try again")
+     *        )
+     *     )
+     * )
+     */
     public function getByTitle(Request $request, $id)
     {
         $movie = Movie::findOrFail($id);
